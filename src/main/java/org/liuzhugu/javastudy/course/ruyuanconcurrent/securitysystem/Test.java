7fdce5463d8e05a54d0da2284484251d7a59d451
liuting <987685625@qq.com>
2021-06-17 09:51:40 +0800

    儒猿 java并发学习第8天 ArrayBlockingQueue

--- src/main/java/org/liuzhugu/javastudy/course/ruyuanconcurrent/securitysystem/Test.java
+++ src/main/java/org/liuzhugu/javastudy/course/ruyuanconcurrent/securitysystem/Test.java
@@ -1,8 +1,11 @@
 package org.liuzhugu.javastudy.course.ruyuanconcurrent.securitysystem;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class Test {
     public static void main(String[] args) {
         AlarmAgent alarmAgent = new AlarmAgent();
         alarmAgent.init();
+
     }
 }
